5b9919a7ced318ed3274e3f15bc7327dbdc1c61f
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-16 22:56:46 +0900

    Create Basic Code For Calculater

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Welcome to Calculator Application");
+
+Console.Write("Enter first number: ");
+var firstNumber = decimal.Parse(Console.ReadLine());
+
+Console.Write("Enter operation that you need ('+' '-' '/' '*' '%'): ");
+var operation = char.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+var secondNumber = decimal.Parse(Console.ReadLine());
+
+var result = operation switch
+{
+    '+' => firstNumber + secondNumber,
+    '-' => firstNumber - secondNumber,
+    '/' => firstNumber / secondNumber,
+    '*' => firstNumber * secondNumber,
+    '%' => firstNumber % secondNumber,
+    _ => throw new Exception("You entered wrong operation, try again")  
+};
+
+Console.WriteLine($"Result: {firstNumber} {operation} {secondNumber} = {result}");
