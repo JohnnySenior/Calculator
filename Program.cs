3dc79823ed17d162e0949ab5a116a47ffdbc3513
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-08-19 17:19:26 +0900

    Refactor-Code

--- Program.cs
+++ Program.cs
@@ -14,33 +14,36 @@ public class Program
             {
                 case 1:
                     decimal firstNumber = ValueManipulatoR.GetInputByMessageToNumbers("Enter first number: ");
-                    string option = ValueManipulatoR.GetInputByMessageToOptions("Option [+, -, *, /, %, sqrt]: ");
-                    if(option == "sqrt")
+                    string option = ValueManipulatoR.GetValidOperation("Option [+, -, *, /, %, sqrt]: ");
+                    if (option.Equals("sqrt"))
                     {
-                        var calculator = new AdvancedCalculator(firstNumber, option);
+                        ICalculatoR calculator = new CalculatoR(firstNumber, option);
                         calculator.Calculate();
                         break;
                     }
                     else
                     {
-                    decimal secondNumber = ValueManipulatoR.GetInputByMessageToNumbers("Enter second number: ");
-                    var calculate = new AdvancedCalculator(firstNumber, option, secondNumber);
-                    calculate.Calculate(); break;
+                        decimal secondNumber = ValueManipulatoR.GetInputByMessageToNumbers("Enter second number: ");
+                        ICalculatoR calculate = new CalculatoR(firstNumber, option, secondNumber);
+                        calculate.Calculate(); break;
                     }
-                case 2: 
+
+                case 2:
                     int age = ValueManipulatoR.GetInputByMessageToAge("Enter your age: ");
-                    var eligibleAge = new EligibleAgeForArmy(age);
+                    IEligibleAgeForArmy eligibleAge = new EligibleAgeForArmy(age);
                     eligibleAge.CheckAgeEligibleForMilitaryService(); break;
 
-                case 3: 
+                case 3:
                     long firstNumberTable = ValueManipulatoR.GetInputByMessageToTable("Enter first number, starting of the table:");
                     long secondNumberTable = ValueManipulatoR.GetInputByMessageToTable("Enter last number, ending of the table:");
                     long inlineFirstNumberTable = ValueManipulatoR.GetInputByMessageToTable("Enter inline first number:");
                     long inlineLastNumberTable = ValueManipulatoR.GetInputByMessageToTable("Enter inline last number:");
-                    var table = new MultiplicationTable(firstNumberTable, secondNumberTable, inlineFirstNumberTable, inlineLastNumberTable);
+                    IMultiplicationTable table = new MultiplicationTable(firstNumberTable, secondNumberTable, inlineFirstNumberTable, inlineLastNumberTable);
                     table.ShowMultiplicationTable(); break;
 
-                case 4: ReporteR.ExitFromProgram(); break;
+                case 4: 
+                    ReporteR.ExitFromProgram(); break;
+
                 default: ReporteR.DefaultCase(); break;
             }
 
